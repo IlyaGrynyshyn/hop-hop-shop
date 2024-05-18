25427f3ec9418623f02ee5fbc3f25208c2c6598c
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-05-18 16:23:30 +0300

    Fix bug: not correct product field

--- shop/models.py
+++ shop/models.py
@@ -7,7 +7,7 @@ from pytils.translit import slugify
 
 def product_image_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
-    filename = f"{slugify(instance.product.title)}-{uuid.uuid4()}{extension}"
+    filename = f"{slugify(instance.product.name)}-{uuid.uuid4()}{extension}"
 
     return os.path.join("uploads/products/", filename)
 
