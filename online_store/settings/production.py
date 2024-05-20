30f3c15a1d3e13cdadbc61981bfb40abb4a278ae
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-05-21 00:53:20 +0300

    Setup production docker-compose file

--- online_store/settings/production.py
+++ online_store/settings/production.py
@@ -1,1 +1,16 @@
+from online_store.settings.base import *
+
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
+
 DEBUG = False
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mydatabase",
+        "USER": "myuser",
+        "PASSWORD": "mypassword",
+        "HOST": "db",
+        "PORT": "5432",
+    }
+}
