485e1ba867b027c7d34f711ab7648b9f3a479686
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-05-21 20:56:13 +0300

    Change endpoint url name & add doc string to EmailSubscriptionView

--- email_subscription/views.py
+++ email_subscription/views.py
@@ -1,8 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from email_subscription.serializers import EmailSubscriptionSerializer
 from email_subscription.models import SubscribedUser
 
 
-class EmailSubscriptionViewSet(viewsets.ModelViewSet):
+class EmailSubscriptionView(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    """
+    Endpoint for processing news subscriptions via email.
+    Supports only POST requests to create new subscriptions.
+    """
+
     queryset = SubscribedUser.objects.all()
     serializer_class = EmailSubscriptionSerializer
--- online_store/urls.py
+++ online_store/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("email-subscription", include("email_subscription.urls")),
+    path("api/email-subscription", include("email_subscription.urls")),
 ]
