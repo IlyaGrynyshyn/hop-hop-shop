bd6d94fd8701f0a82cc1a19facde95ac4709aa06
Mary-Dan <152028992+Mary-Dan@users.noreply.github.com>
2024-05-21 14:22:24 +0300

    убрала -r

--- shop/urls.py
+++ shop/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import CategoryViewSet, ProductViewSet
 
 router = DefaultRouter()
-router.register(r'categories', CategoryViewSet)
-router.register(r'products', ProductViewSet)
+router.register('categories', CategoryViewSet)
+router.register('products', ProductViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
