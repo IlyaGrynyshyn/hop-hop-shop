945f596db9c4681f7eff4ef5630564ce56da8fe5
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-06-05 16:36:13 +0300

    Implement urls for get login tokens

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,10 +3,18 @@ from authentication.views import (
     CreateCustomerView,
     ManageUserView,
 )
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 
 app_name: str = "customer"
 urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("registration/", CreateCustomerView.as_view(), name="create"),
-    path("me/", ManageUserView.as_view(), name="manage"),
+    path("profile/", ManageUserView.as_view(), name="profile"),
 ]
