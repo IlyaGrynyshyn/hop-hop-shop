17510e6021e05d372c6f9da921fb965d930bd5ad
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-06-27 23:12:22 +0300

    Add url for coupon

--- cart/urls.py
+++ cart/urls.py
@@ -1,24 +1,28 @@
 from django.urls import path
-from .views import (
+from cart.views import (
     CartAddItemView,
     CartRemoveItemView,
     CartDetailView,
     CartSubtractItemView,
+    CouponVIewView,
+    RemoveCoupon,
 )
 
 app_name = "cart"
 
 urlpatterns = [
     path("", CartDetailView.as_view(), name="cart_detail"),
-    path("cart/add/<int:product_id>/", CartAddItemView.as_view(), name="cart_add"),
+    path("add/<int:product_id>/", CartAddItemView.as_view(), name="cart_add"),
     path(
-        "cart/remove/<int:product_id>/",
+        "remove/<int:product_id>/",
         CartRemoveItemView.as_view(),
         name="cart_remove",
     ),
     path(
-        "cart/subtract/<int:product_id>/",
+        "subtract/<int:product_id>/",
         CartSubtractItemView.as_view(),
         name="cart_subtract",
     ),
+    path("coupon/", CouponVIewView.as_view(), name="coupon"),
+    path("coupon/remove/", RemoveCoupon.as_view(), name="coupon_remove"),
 ]
