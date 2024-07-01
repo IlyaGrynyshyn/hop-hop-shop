d3c7e1eecb3c67da11f73cdbbb231a1642377847
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-07-01 18:12:40 +0300

    Remove django debug toolbar

--- online_store/settings.py
+++ online_store/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework_simplejwt",
     "rest_framework",
-    "debug_toolbar",
     "drf_standardized_errors",
     "django_filters",
     "corsheaders",
@@ -61,7 +60,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "cart.middleware.CartTransferMiddleware",
 ]
 
