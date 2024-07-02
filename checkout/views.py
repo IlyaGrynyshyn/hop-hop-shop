9914be77b769162fdb6dcd18f78e1180415be478
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-07-02 23:24:06 +0300

    refactor cart items initialize

--- checkout/views.py
+++ checkout/views.py
@@ -13,32 +13,9 @@ class CheckoutView(generics.CreateAPIView):
             return Response(
                 {"error": "Your cart is empty"}, status=status.HTTP_400_BAD_REQUEST
             )
-        items_data = [
-            {
-                "product": item["product"]["id"],
-                "quantity": item["quantity"],
-                "price": item["price"],
-            }
-            for item in cart
-        ]
-        order_data = {**request.data, "items": items_data}
-        serializer = OrderSerializer(data=order_data)
+
+        serializer = OrderSerializer(data=request.data, context={"request": request})
         if serializer.is_valid():
             serializer.save()
-            cart.clear()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def perform_create(self, serializer):
-        cart = CartSessionService(self.request)
-
-        items_data = [
-            {
-                "product": item["product"]["id"],
-                "quantity": item["quantity"],
-                "price": item["price"],
-            }
-            for item in cart
-        ]
-
-        serializer.save(items=items_data)
