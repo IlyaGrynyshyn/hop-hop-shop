a7eeeab3435202d17f3f7ec2d5a8587a2bf901f9
LazebnyA <91191669+LazebnyA@users.noreply.github.com>
2024-09-11 17:03:13 +0300

    auth: changed reset password, implementation retrieving user by email instead of id (#22)

--- authentication/utils.py
+++ authentication/utils.py
@@ -3,8 +3,8 @@ import os
 from django.core.mail import send_mail
 
 
-def send_reset_password_email(user_id, token, email):
-    reset_url = f"{os.environ['PASSWORD_RESET_BASE_URL']}&token={token}&id={user_id}"
+def send_reset_password_email(token, email):
+    reset_url = f"{os.environ['PASSWORD_RESET_BASE_URL']}?key={token}&user_email={email}"
 
     subject = "Password reset"
     from_email = "no-replay@hop-hop-shop.me"
