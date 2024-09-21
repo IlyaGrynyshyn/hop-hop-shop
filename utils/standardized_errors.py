79e45c27916e5b38756b7031b52dbf8f08c82682
LazebnyA <oleksandrlazebnij8@gmail.com>
2024-09-21 16:21:27 +0300

    errors/auth: change validation errors response for non field errors

--- utils/standardized_errors.py
+++ utils/standardized_errors.py
@@ -6,9 +6,16 @@ from drf_standardized_errors.types import ErrorResponse
 class MyExceptionFormatter(ExceptionFormatter):
     def format_error_response(self, error_response: ErrorResponse):
         error = error_response.errors[0]
+        if (error_response.type == "validation_error"
+                and error.attr != "non_field_errors"
+                and error.attr is not None
+                and error.detail[:4] == 'This'):
+            error_message = error.detail.replace('This', error.attr)
+        else:
+            error_message = error.detail
         return {
             "success": False,
             "error": {
-                "message": error.detail
+                "message": error_message
             }
         }
