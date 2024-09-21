c23cd014a424fc6a3a9ebe347458a13f4306dca2
LazebnyA <oleksandrlazebnij8@gmail.com>
2024-09-21 16:47:00 +0300

    errors: return list of required fields instead of first one

--- utils/standardized_errors.py
+++ utils/standardized_errors.py
@@ -5,14 +5,21 @@ from drf_standardized_errors.types import ErrorResponse
 
 class MyExceptionFormatter(ExceptionFormatter):
     def format_error_response(self, error_response: ErrorResponse):
-        error = error_response.errors[0]
-        if (error_response.type == "validation_error"
+        required_field_errors = [
+            error for error in error_response.errors
+            if (error_response.type == "validation_error"
                 and error.attr != "non_field_errors"
                 and error.attr is not None
-                and error.detail[:4] == 'This'):
-            error_message = error.detail.replace('This', error.attr)
+                and error.detail.startswith('This'))
+        ]
+
+        if required_field_errors:
+            required_fields = ', '.join([error.attr for error in required_field_errors])
+            field_term = "field is" if len(required_field_errors) == 1 else "fields are"
+            error_message = f"{required_fields} {field_term} required"
         else:
-            error_message = error.detail
+            error_message = error_response.errors[0].detail
+
         return {
             "success": False,
             "error": {
